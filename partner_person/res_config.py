04f5c69b98578ef32ddfeaa1ae72a24018959e6b
Pierre Emmanuel DOUET (Tathar) <tathar.dev@gmail.com>
2015-02-12 19:33:28 +0100

    correct bug in res_config.py

--- partner_person/res_config.py
+++ partner_person/res_config.py
@@ -31,10 +31,10 @@ class partner_configuration(osv.osv_memory):
             implied_group='partner_person.person_nationality',
             help="Show Nationality Field in Personal Information Tab"),        
         'group_medical_certificate': fields.boolean("Show Medical Certificate Information",
-            implied_group='person_medical_certificate',
+            implied_group='partner_person.person_medical_certificate',
             help="Show Medical Certificate Field in Personal Information Tab"),
         'group_image_rights': fields.boolean("Show Image Rights Information",
-            implied_group='person_image_rights',
+            implied_group='partner_person.person_image_rights',
             help="Show Image Rights Field in Personal Information Tab"),
     }
 
